1e7986a95d51911c0f1f19b619ce6d0700f9ec57
tingtang <tkjoetang@yahoo.com>
2019-07-22 23:12:59 -0700

    add run script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+CONTAINSER_NAME='seeingtheworld'
+IMAGES_NAME='aiformankind/seeingtheworld'
+
+is_images_exist=$(docker images -a | grep "$IMAGES_NAME")
+if [ ! -z is_images_exist ]; then
+  docker build -t $IMAGES_NAME:0.0.1 .
+fi
+
+is_container_exist=$(docker ps | grep "$CONTAINSER_NAME")
+if [ -z "$is_container_exist" ]; then
+  docker run -i -p 8888:8888 -p 6006:6006 --name=$CONTAINSER_NAME $IMAGES_NAME:0.0.1
+else
+  docker stop $CONTAINSER_NAME
+  docker rm $CONTAINSER_NAME
+  docker run -i -p 8888:8888 -p 6006:6006 --name=$CONTAINSER_NAME $IMAGES_NAME:0.0.1
+fi
