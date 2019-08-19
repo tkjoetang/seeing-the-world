b3f189208aff2c2a9136ae0247eaedb15982412a
Andy Tang <andylmtang@gmail.com>
2019-08-18 21:58:34 -0700

    Optimize container usage

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-CONTAINSER_NAME='seeingtheworld'
+CONTAINER_NAME='seeingtheworld'
 IMAGES_NAME='aiformankind/seeingtheworld'
 
 is_images_exist=$(docker images -a | grep "$IMAGES_NAME")
@@ -8,11 +8,11 @@ if [ ! -z is_images_exist ]; then
   docker build -t $IMAGES_NAME:0.0.1 --build-arg TRAING_STEPS=1000 .
 fi
 
-is_container_exist=$(docker ps | grep "$CONTAINSER_NAME")
+is_container_exist=$(docker ps | grep "$CONTAINER_NAME")
 if [ -z "$is_container_exist" ]; then
-  docker run -i -p 8888:8888 -p 6006:6006 --name=$CONTAINSER_NAME $IMAGES_NAME:0.0.1
+  docker run -i -p 8888:8888 -p 6006:6006 -v `pwd`/data:/app/data --name=$CONTAINER_NAME $IMAGES_NAME:0.0.1
 else
-  docker stop $CONTAINSER_NAME
-  docker rm $CONTAINSER_NAME
-  docker run -i -p 8888:8888 -p 6006:6006 --name=$CONTAINSER_NAME $IMAGES_NAME:0.0.1
+  docker stop $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
+  docker run -i -p 8888:8888 -p 6006:6006 -v `pwd`/data:/app/data --name=$CONTAINER_NAME $IMAGES_NAME:0.0.1
 fi
